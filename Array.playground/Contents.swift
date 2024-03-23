05e3a85338bb2563eba77e8cca59f2b50c8f6289
Mohammad Ali Al Biruni Akash Sharkar <akashsharkar786@gmail.com>
2024-03-23 01:20:09 -0700

    Initial Commit

---
+++ Array.playground/Contents.swift
@@ -0,0 +1,39 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+
+//Topic: Datastructure: Array
+
+var shopingCart = [String]()
+var name = ["Robindro book","Gold life","belt","Mackbook pro"]
+var Price = [120,17,200,150000]
+
+
+// add any item in array
+shopingCart.append("Office Chair")
+shopingCart.append("AirPords")
+
+
+// Check array .
+
+let product1 = name[0]
+let product2 = name[1]
+
+//remove any item from array ...
+
+name.remove(at: 0)
+name
+
+
+Price.removeFirst()
+Price
+Price.removeLast()
+Price
+
+
+//Insert any item in array .
+name.insert("Sun glass", at: 0)
+name.insert("hand watch", at: 1)
+
+
