4d3e6f167856fd059472aff295e61689cab87aa8
Mohammad Ali Al Biruni Akash Sharkar <akashsharkar786@gmail.com>
2024-03-23 02:18:34 -0700

    first commit

--- Dictionaryes.playground/Contents.swift
+++ Dictionaryes.playground/Contents.swift
@@ -1,3 +1,16 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+var nameToEmailDict = ["akash":"akash@gmail.com","dola":"dola@gmail.com","munna":"munna@gmail.com"]
+var blankDict = [String:String]()
+var nameToAgeDict = ["Ony":23,"Shilpi":22]
+
+// checking dictionary value
+
+let akashEmail = nameToEmailDict["akash"]
+let onyAge = nameToAgeDict["Ony"]
+
+nameToEmailDict["akash"] = "akash@gmail.com"
+nameToEmailDict
+
